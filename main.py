62ea459aa5390f9004b98e60a9cf4b7a6bd58b7e
Harsh  Kapoor <hkapoordroid@gmail.com>
2017-05-28 20:17:39 -0700

    added table and mval, tval, rval support

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # [START app]
 import logging
 import urllib2
+import csv
+import StringIO
+import ast
 
 # [START imports]
 from flask import Flask, render_template, request
@@ -24,18 +27,38 @@ def submitted_form():
     company = request.form['company']
     investment = request.form['investment']
     runat = request.form['runat']
-    rtype = request.form['rtype']
+    tval = request.form['tval']
+    mval = request.form['mval']
+    rval = 100
+
+    #read the csv file to get the adj close range
+    csvfileurl = 'https://s3.eu-west-2.amazonaws.com/flelliotvar0/'+company+'.csv'
+    print(csvfileurl)
+    rcsv = urllib2.urlopen(csvfileurl)
+    csvstream = StringIO.StringIO(rcsv.read())
+    csvdata = list(csv.reader(csvstream))
+    adj_close = [float(row[6]) for row in csvdata[1:]]
+
+
+
 
     if runat == 'lambda':
-    	r = urllib2.urlopen('http://6pi4zde07i.execute-api.eu-west-2.amazonaws.com/Prod?company='+str(company)+'&investment='+str(investment)+'&rtype='+str(rtype))
+    	r = urllib2.urlopen('http://6pi4zde07i.execute-api.eu-west-2.amazonaws.com/Prod?company='+str(company)+'&investment='+str(investment)+'&rval=100&tval='+str(tval)+'&mval='+str(mval))
     else:
-        r = urllib2.urlopen('http://ec2-35-176-60-162.eu-west-2.compute.amazonaws.com/?company='+str(company)+'&investment='+str(investment)+'&rtype='+str(rtype))
+        r = urllib2.urlopen('http://ec2-35-176-60-162.eu-west-2.compute.amazonaws.com/?company='+str(company)+'&investment='+str(investment)+'&rval=100&tval='+str(tval)+'&mval='+str(mval))
+
+    rdict = ast.literal_eval(r.read())
 
     # [END submitted]
     # [START render_template]
     return render_template(
-        'form.html',
-        result=r.read())
+        'result.html',
+        historic95 = "%.2f" % rdict['historical'][0],
+	historic99 = "%.2f" % rdict['historical'][1],
+	covariance95 = "%.2f" % rdict['covariance'][0],
+	covariance99 = "%.2f" % rdict['covariance'][1],
+	montecarlo95 = "%.2f" % rdict['montecarlo'][0],
+	montecarlo99 = "%.2f" % rdict['montecarlo'][1])
     # [END render_template]
 
 
